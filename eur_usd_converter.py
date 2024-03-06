85606bba549a0258b20e81f9b93b7394dbd834aa
Antons Pidgora <you@example.com>
2024-03-06 13:04:08 +0200

    asda

--- eur_usd_converter.py
+++ eur_usd_converter.py
@@ -2,23 +2,24 @@
 # Mērķis: 
 # iespēja konvertēt USD uz EUR un otrādi pēc 1.08 kursa
 
-eur_to_usd_rate = 1.09
+eur_to_usd_rate = 1.08
+
 
 print("Choose command")
 while True:
     print("=======")
-    print("1 - convert EUR into USD")
-    print("2 - convert USD into EUR")
+    print("1 - convert USD into EUR")
+    print("2 - convert EUR into USD")
     print("3 - exit")
     command = input("")
 
     if command == "1":
-        usd = int(input("Enter USD:"))
-        eur = usd / 1.09
+        usd = float(input("Enter USD:"))
+        eur = usd / eur_to_usd_rate
         print(usd, "USD equals EUR", eur)
     elif command == "2":
-        usd = int(input("Enter EUR:"))
-        eur = usd * 1.09
+        usd = float(input("Enter EUR:"))
+        eur = usd * eur_to_usd_rate
         print(usd, "USD equals EUR", eur)
     else:
         break

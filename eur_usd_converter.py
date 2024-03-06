cf93a130de1d1a58e41bb8239c43c3f9e24c7b96
Antons Pidgora <you@example.com>
2024-03-06 12:19:12 +0200

    asda

--- eur_usd_converter.py
+++ eur_usd_converter.py
@@ -5,19 +5,19 @@
 eur_to_usd_rate = 1.09
 
 print("Choose command")
-while False:
+while True:
     print("=======")
     print("1 - convert EUR into USD")
     print("2 - convert USD into EUR")
     print("3 - exit")
     command = input("")
 
-    if command == 1:
-        usd = input("Enter USD:")
+    if command == "1":
+        usd = int(input("Enter USD:"))
         eur = usd / 1.09
         print(usd, "USD equals EUR", eur)
-    elif command == 2:
-        usd = input("Enter EUR:")
+    elif command == "2":
+        usd = int(input("Enter EUR:"))
         eur = usd * 1.09
         print(usd, "USD equals EUR", eur)
     else:
